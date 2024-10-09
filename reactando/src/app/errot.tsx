435b6c01bd93a50b08b38f2e5e3f839ba9aea72a
Enzo Soddano <enzosoddano1203@gmail.com>
2024-10-09 02:38:32 -0300

    Criar NotExpectedErrorPage

---
+++ reactando/src/app/errot.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+const NotExpectedErrorPage = () => {
+
+    return (
+
+        <div className="flex items-center justify-center h-screen">
+
+            <div className="text-center">
+
+                <h1 className="text-4x1 font-bold text-white-900">500</h1>
+
+                <p className="text-white-700">Not Expected Error</p>
+
+            </div>
+
+        </div>
+
+    )
+
+}
+
+export default NotExpectedErrorPage
