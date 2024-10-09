504cb7c786cdd07a5513012fe79e690c2cda0705
Enzo Soddano <enzosoddano1203@gmail.com>
2024-10-09 14:47:40 -0300

    Atribuir função ao botão

--- reactando/src/components/product/ProductsListNextPage.tsx
+++ reactando/src/components/product/ProductsListNextPage.tsx
@@ -66,7 +66,7 @@ const ProductsListNextPageComponent = ({ perPage = 12 }: ProductsListProps) => {
 
                     hasMore && (
 
-                        <button className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${loading ? 'opacity-58 cursor-not-allowed' : ''}`}>
+                        <button onClick={getData} className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${loading ? 'opacity-58 cursor-not-allowed' : ''}`}>
 
                             {loading ? 'loading...' : 'Load More'}
 
